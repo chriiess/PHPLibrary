efca5bacc0c69f3c55e4cdc5c28e6627c4c30fb4
levi <455640637@qq.com>
2016-09-22 18:21:38 +0800

    代码片段更新

--- wechat/functions.php
+++ wechat/functions.php
@@ -1,123 +1,4 @@
 <?php
-
-/**
- * #######################################
- *
- * 字符串处理相关代码片段
- *
- * #######################################
- */
-
-/**
- * 等宽截取字符串函数
- * @description
- *  一个中文字符代表两个长度,一个英文字符代表一个长度
- * @author gwing
- * @since 2016-09-02
- * @param string $str 要截取的字符串
- * @param int $length 要截取的长度
- * @param int $start 要截取的起始位置
- * @param string $suffix 自定义后缀
- * @return string $new_str 返回截取的字符串
- */
-
-function msubstr($str, $length, $start = 0, $suffix = "") {
-
-    $new_str = "";
-    $str_length = $length + $start;
-
-    for ($i = $start; $i < $str_length; $i++) {
-
-        if (ord(mb_substr($str, $i, 1, 'utf-8')) > 0xa0) {
-            $str_length -= 1;
-        }
-
-        $new_str .= mb_substr($str, $i, 1, 'utf-8');
-    }
-
-    return $new_str . $suffix;
-}
-
-/**
- * 产生随机字符串
- *
- * @param    int        $length  输出长度
- * @param    string     $chars   可选的 ，默认为 0123456789
- * @return   string     字符串
- */
-function random($length, $chars = '123456789abcdefghijklmnpqrstuvwxyzABCDEFGHIJKLMNPQRSTUVWXYZ') {
-    $hash = '';
-    $max = strlen($chars) - 1;
-    for ($i = 0; $i < $length; $i++) {
-        $hash .= $chars[mt_rand(0, $max)];
-    }
-    return $hash;
-}
-
-/**
- * 截取中文UTF-8字符串
- *
- * @param string $str   要截取的字符串
- * @param string $start 中文UTF-8字符串的起始位置
- * @param int    $lenth 要截取中文UTF-8字符串的长度
- * @return string
- */
-function utf8Substr($str, $start, $lenth) {
-    $len = strlen($str);
-    $r = array();
-    $n = 0;
-    $m = 0;
-    for ($i = 0; $i < $len; $i++) {
-        $x = substr($str, $i, 1);
-        $a = base_convert(ord($x), 10, 2);
-        $a = substr('00000000' . $a, -8);
-        if ($n < $start) {
-            if (substr($a, 0, 1) == 0) {
-            } elseif (substr($a, 0, 3) == 110) {
-                $i += 1;
-            } elseif (substr($a, 0, 4) == 1110) {
-                $i += 2;
-            }
-            $n++;
-        } else {
-            if (substr($a, 0, 1) == 0) {
-                $r[] = substr($str, $i, 1);
-            } elseif (substr($a, 0, 3) == 110) {
-                $r[] = substr($str, $i, 2);
-                $i += 1;
-            } elseif (substr($a, 0, 4) == 1110) {
-                $r[] = substr($str, $i, 3);
-                $i += 2;
-            } else {
-                $r[] = '';
-            }
-            if (++$m >= $lenth) {
-                break;
-            }
-        }
-    }
-    $r = implode('', $r);
-    return $r;
-}
-
-/**
- * 统计utf8中文字符串长度的函数
- *
- * @param string $str 要计算长度的字符串
- * @return int        返回字符串的长度
- */
-function utf8Strlen($str) {
-    if (empty($str)) {
-        return 0;
-    }
-    if (function_exists('mb_strlen')) {
-        return mb_strlen($str, 'utf-8');
-    } else {
-        preg_match_all("/./u", $str, $ar);
-        return count($ar[0]);
-    }
-}
-
 /**
  * 字符截取 支持UTF8/GBK
  * @param $string
@@ -193,34 +74,6 @@ function strCut($string, $length, $dot = '...') {
 }
 
 /**
- * xss过滤函数
- *
- * @param $string
- * @return string
- */
-function removeXss($string) {
-    $string = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S', '', $string);
-    $parm1 = Array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
-    $parm2 = Array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
-    $parm = array_merge($parm1, $parm2);
-    for ($i = 0; $i < sizeof($parm); $i++) {
-        $pattern = '/';
-        for ($j = 0; $j < strlen($parm[$i]); $j++) {
-            if ($j > 0) {
-                $pattern .= '(';
-                $pattern .= '(&#[x|X]0([9][a][b]);?)?';
-                $pattern .= '|(&#0([9][10][13]);?)?';
-                $pattern .= ')?';
-            }
-            $pattern .= $parm[$i][$j];
-        }
-        $pattern .= '/i';
-        $string = preg_replace($pattern, '', $string);
-    }
-    return $string;
-}
-
-/**
  * 清除html、css、js格式并去除空格
  */
 function ClearHtml($descclear) {
@@ -258,4 +111,26 @@ function ClearHtml($descclear) {
     $descclear = preg_replace($pat, "", $descclear);
 
     return $descclear;
+}
+
+/**
+ * CURL GET
+ */
+function Curl_get($url) {
+    //初始化
+    $curl = curl_init();
+    //设置抓取的url
+    curl_setopt($curl, CURLOPT_URL, $url);
+    //设置头文件的信息作为数据流输出
+    curl_setopt($curl, CURLOPT_HEADER, 0);
+    //设置获取的信息以文件流的形式返回，而不是直接输出。
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //只需要设置一个秒的数量就可以
+    curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+    //执行命令
+    $data = curl_exec($curl);
+    //关闭URL请求
+    curl_close($curl);
+    //显示获得的数据
+    return $data;
 }
